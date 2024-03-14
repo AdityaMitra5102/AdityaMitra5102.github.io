af1b390a6244edff75d69ab7dfc9fcc37d1be132
Aditya Mitra <adityaarghya0@gmail.com>
2024-03-14 08:26:46 +0530

    Update track.js

--- track.js
+++ track.js
@@ -10,8 +10,8 @@ fetch('https://api.ipify.org?format=json')
 	.then(response => response.json())
 	.then(data => {
 
-	msg2=JSON.stringify(data);
-	msg=msg2+msg;
+	msg2=JSON.stringify(data, null, 2);
+	msg=msg+"\n"+msg2;
 	
 	url='https://api.telegram.org/bot7116418976:AAHHMdxfAshkZPzu5wwTLf7xerSl64BXiXg/sendMessage?chat_id=634841328&text='+encodeURI(msg);
 	fetch(url).then(console.log("Done"));
