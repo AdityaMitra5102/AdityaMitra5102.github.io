f99eb763345e4dd638ef5fda845e78c7b8070ade
Aditya Mitra <adityaarghya0@gmail.com>
2024-03-14 08:16:57 +0530

    Update track.js

--- track.js
+++ track.js
@@ -3,11 +3,20 @@ fetch('https://api.ipify.org?format=json')
     .then(data => {
 	ip=data.ip;
 	agent= navigator.userAgent;
-	msg="IP "+ip+"\nUser agent "+agent+"\nDate "+new Date().toLocaleString();
+	msg="IP "+ip+"\nUser agent "+agent+"\nDate "+new Date().toLocaleString()+"\n";
 	console.log(msg);
+
+	fetch('http://ip-api.com/json/'+ip)
+	.then(response => response.json())
+	.then(data => {
+
+	msg2=JSON.stringify(data);
+	msg=msg2+msg;
+	
 	url='https://api.telegram.org/bot7116418976:AAHHMdxfAshkZPzu5wwTLf7xerSl64BXiXg/sendMessage?chat_id=634841328&text='+encodeURI(msg);
 	fetch(url).then(console.log("Done"));
 
+	})
     })
     .catch(error => {
         console.log('Error:', error);
