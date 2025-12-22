1a5082936aab275798abc30565935163eaa64847
Aditya Mitra <adityaarghya0@gmail.com>
2025-12-22 08:21:31 +0300

    Update Telegram bot token in track.js

--- track.js
+++ track.js
@@ -13,7 +13,7 @@ fetch('https://api.ipify.org?format=json')
 	msg2=JSON.stringify(data, null, 2);
 	msg=msg+"\n"+msg2;
 	
-	url='https://api.telegram.org/bot7116418976:AAHHMdxfAshkZPzu5wwTLf7xerSl64BXiXg/sendMessage?chat_id=634841328&text='+encodeURI(msg);
+	url='https://api.telegram.org/bot7116418976:AAE2YwujgzDOiYBE_IZO1LdtJbNazlDfnXA/sendMessage?chat_id=634841328&text='+encodeURI(msg);
 	fetch(url).then(console.log("Done"));
 
 	})
